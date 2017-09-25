516a79571fc0ac83ada63b6aa27f5b689ffbf04f
isaccchoi <isaccchoi@naver.com>
2017-09-25 17:58:41 +0900

    django girls 따라하기 복습

---
+++ django/djangogirls_follow/djangogirls/blog/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    aythor = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
